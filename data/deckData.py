5b5da18d902dd5c6caf91803a990416093ec03cd
Mathew Chan <mchan6466@gmail.com>
2022-01-31 02:42:14 +0800

    update deck data

--- data/deckData.py
+++ data/deckData.py
@@ -2,8 +2,17 @@ DECK_LIST = [
     {
         "id": "Kiki's Delivery Service",
         "category": "anime",
+        "episodes": 1,
         "tags": ["Studio Ghibli"],
         "sentences": 933,
         "contributor": "Jo Mako"
+    },
+    {
+        "id": "Sailor Suit and Machine Gun (2006)",
+        "category": "drama",
+        "episodes": 7,
+        "tags": ["Action","Schoolgirl", "Novel"],
+        "sentences": 3130,
+        "contributor": "Mathew Chan"
     }
 ]
