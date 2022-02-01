1ea827a5a9d849965d630d57bf051a8e5cc99061
Mathew Chan <mchan6466@gmail.com>
2022-02-01 15:59:39 +0800

    update deck data

--- data/deckData.py
+++ data/deckData.py
@@ -1,18 +1,84 @@
+# Ensure each deck id is unique across categories
+
 DECK_LIST = [
     {
-        "id": "Kiki's Delivery Service",
+        "id": "Castle in the Sky",
+        "ja_name": "天空の城ラピュタ", 
         "category": "anime",
         "episodes": 1,
+        "year": 1986,
+        "tags": ["Studio Ghibli"],
+        "sentences": 957,
+        "contributor": "Jo Mako"
+    },
+    {
+        "id": "Howl's Moving Castle",
+        "ja_name": "ハウルの動く城", 
+        "category": "anime",
+        "episodes": 1,
+        "year": 2004,
+        "tags": ["Studio Ghibli"],
+        "sentences": 931,
+        "contributor": "Jo Mako"
+    },
+    {
+        "id": "Kiki's Delivery Service",
+        "ja_name": "魔女の宅急便", 
+        "category": "anime",
+        "episodes": 1,
+        "year":1990,
         "tags": ["Studio Ghibli"],
         "sentences": 933,
         "contributor": "Jo Mako"
     },
     {
+        "id": "My Neighbor Totoro",
+        "ja_name": "となりのトトロ", 
+        "category": "anime",
+        "episodes": 1,
+        "year": 1988,
+        "tags": ["Studio Ghibli"],
+        "sentences": 474,
+        "contributor": "Jo Mako"
+    },
+    {
         "id": "Sailor Suit and Machine Gun (2006)",
+        "ja_name": "セーラー服と機関銃", 
         "category": "drama",
         "episodes": 7,
+        "year": 2006,
         "tags": ["Action","Schoolgirl", "Novel"],
         "sentences": 3130,
         "contributor": "Mathew Chan"
+    },
+    {
+        "id": "Spirited Away",
+        "ja_name": "千と千尋の神隠し", 
+        "category": "anime",
+        "episodes": 1,
+        "year": 2001,
+        "tags": ["Studio Ghibli"],
+        "sentences": 1038,
+        "contributor": "Jo Mako"
+    },
+     {
+        "id": "Spirited Away",
+        "ja_name": "猫の恩返し", 
+        "category": "anime",
+        "episodes": 1,
+        "year": 2002,
+        "tags": ["Studio Ghibli"],
+        "sentences": 830,
+        "contributor": "Jo Mako"
+    },
+     {
+        "id": "When Marnie Was There",
+        "ja_name": "思い出のマーニー", 
+        "category": "anime",
+        "episodes": 1,
+        "year": 2002,
+        "tags": ["Studio Ghibli"],
+        "sentences": 830,
+        "contributor": "Jo Mako"
     }
 ]
